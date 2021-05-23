eacc2c6d35e0808f332d65cd65b0bfa3c9357940
ishika kesarwani <>
2021-05-23 18:47:13 +0000

    add

---
+++ Day1_Band_name_generator/main.py
@@ -0,0 +1,5 @@
+print("Hey there! Welcome to the band name generator!!\n")
+
+city=input("Which city did you grow up in?\n")
+pet = input("Whats your first pet's name?\n")
+print("Your band name could be " + city +" "+pet)
